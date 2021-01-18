c2554cda525a0d55c0b3d6c9a5c0ce6d4858e6a1
halibotemovie2 <77602301+halibotemovie2@users.noreply.github.com>
2021-01-18 15:59:06 +1100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    // main method
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("second line");
+        System.out.println();
+        // escape sequences:  \" \\
+        System.out.println("this is \\\\\\ a so-called \"fourth\" line");
+    }
+}
