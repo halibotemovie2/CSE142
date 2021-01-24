8da7a96fd4fa468006a23e332c8bb258f5a3bd47
halibotemovie2 <77602301+halibotemovie2@users.noreply.github.com>
2021-01-24 14:58:51 +1100

    Add files via upload

---
+++ Foo.java
@@ -0,0 +1,11 @@
+public class Foo {
+    public String toString() {
+        return "foo";
+    }
+    public void method1() {
+        System.out.println("foo 1"); }
+    public void method2() {
+        System.out.println("foo 2"); }
+}
+
+
